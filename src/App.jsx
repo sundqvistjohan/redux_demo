5a392d421aff96153d4fb6c1a1f480fa7b40db28
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-01-16 14:16:35 +0100

    Adds functionality to change message

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,48 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
 class App extends Component {
+
+  changeMessage = event => {
+    event.preventDefault()
+    let newMessage = event.target.newMessage.value
+    event.target.newMessage.value = ''
+    this.props.changeMessage(newMessage)
+  }
+
   render() {
     return (
       <>
-        {this.props.message}
+        <h1>{this.props.message}</h1>
+        <form onSubmit={this.changeMessage}>
+          <input type="text" name="newMessage" placeholder="Type a message" />
+          <input type="submit" value="Change message" />
+        </form>
+        <button
+          onClick={() => this.props.changeMessage('Hello Galaxy')}
+        >
+          Change message
+        </button>
+        <button
+          onClick={() => this.props.resetMessage}
+        >
+          Reset
+        </button>
       </>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     message: state.message
   }
 }
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => {
+  return {
+    resetMessage: () => {dispatch({type: 'RESET_MESSAGE'})},
+    changeMessage: message => {dispatch({type: 'CHANGE_MESSAGE', payload: message })}
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
